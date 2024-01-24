4e02bc414daa40ab65ca85ac716c7b65de1f1094
Hemannachowdari <hemanna1323@gmail.com>
2024-01-24 17:55:59 +0530

    major changes

---
+++ Bsp/Pala/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from Pala import views
+
+
+urlpatterns = [
+    path('s1/',views.s1),
+    path('s2/',views.s2),
+    path('t/',views.data_time),
+    path('',views.home),
+    path('add',views.add),
+    path('emp/',views.emp_details),
+]
---
+++ Bsp/Pala/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+def s1(request):
+    return HttpResponse("<h1>Hemannachowdari")
+
+def s2(request):
+    return HttpResponse("Palabandla")
+
+def data_time(request):
+    time = datetime.datetime.now()
+    t = '<h2>Current datetime is:'+str(time)+'</h2>'
+    return HttpResponse(t)
+
+def home(request):
+    return render(request, 'home.html',context={'name':"Hemannachowdari"})
+
+def add(request):
+    a = int(request.POST['num1'])
+    b = int(request.POST['num2'])
+    result = a+b
+    return render(request,('result.html',{'result':result }))
+
+def emp_details(request):
+    ename ="Hemannachowdari"
+    eid = 34
+    esal =343534
+    eadd ="Basampalli"
+    designation ="Software Engineer"
+
+    context_details ={"ename":ename,"eid":eid,"esal":esal,"eadd":eadd,"designation":designation}
+    return render(request, 'emp.html',context_details)
+
+def my_details(request):
+    name ="Hemannachowdari"
+    sur_name ="Palabandla"
+    add ="Basampalli"
+    designation ="Software Engineer"
+
+# def something(request):
+#     pass
